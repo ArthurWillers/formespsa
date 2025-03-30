971bcd340d06ab6b9a8b9aef215ea579ff6b1b80
Arthur Willers <arthur2008willers@gmail.com>
2025-03-30 11:19:39 -0300

    Update title of futsal registration form

--- forms/form_futsal.php
+++ forms/form_futsal.php
@@ -37,7 +37,7 @@ if (isset($_POST['submitFutsal'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="../assets/logoPrefeituraSA.png" type="image/x-icon">
-  <title>Formulário: Inscrição para o Festival de Esportes</title>
+  <title>Futsal</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
